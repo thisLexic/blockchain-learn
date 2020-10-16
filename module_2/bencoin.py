6a4a7cea6db3a190c4e3e2f3ee028007d5f5f2d9
thisLexic <benvillabroza1999@gmail.com>
2020-10-16 17:20:36 +0800

    added the return in the replace_chain function

--- module_2/bencoin.py
+++ module_2/bencoin.py
@@ -213,6 +213,7 @@ def replace_chain():
             'message': 'Chain is the longest chain in the network',
             'chain': blockchain.chain,
         }
+    return jsonify(response), 200
 
 
 app.run(host='0.0.0.0', port=5000)
