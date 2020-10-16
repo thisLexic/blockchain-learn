411b4131b2c34acae47b97ce2b401e4f82c167b7
thisLexic <benvillabroza1999@gmail.com>
2020-10-16 17:19:39 +0800

    created api call for checking the longest chain in the network

--- module_2/bencoin.py
+++ module_2/bencoin.py
@@ -199,5 +199,20 @@ def connect_node():
     }
     return jsonify(response), 201
 
+# replacing the chain by the longest chain (if needed)
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {
+            'message': 'Chain replaced by the longest chain in the network',
+            'chain': blockchain.chain,
+        }
+    else:
+        response = {
+            'message': 'Chain is the longest chain in the network',
+            'chain': blockchain.chain,
+        }
+
 
 app.run(host='0.0.0.0', port=5000)
